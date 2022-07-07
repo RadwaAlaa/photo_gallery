bbeedc0102b5085018f8e1ca39cba860c05c7daa
Radwa Alaa <eng_radwa_alaa@yahoo.com>
2022-07-07 20:58:48 +0200

    presentation

---
+++ lib/features/home/presentation/pages/home_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:photo_gallery/features/home/presentation/cubit/home_cubit.dart';
+import 'package:photo_gallery/features/home/presentation/widgets/carousel_banner_widget.dart';
+import 'package:photo_gallery/features/home/presentation/widgets/venues_list_widget.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: BlocBuilder<HomeCubit, HomeState>(builder: (context, state) {
+      if (state is BannersLoaded) {
+        return Column(
+          children: [
+            CarouselBanner(banners: state.banners),
+            Expanded(child: VenuesList(venues: state.venues))
+          ],
+        );
+      } else {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+    }));
+  }
+}
